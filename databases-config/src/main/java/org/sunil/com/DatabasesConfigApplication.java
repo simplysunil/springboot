cab7576430484ef921d366610bcfec9a521566f0
sunil <sunilit01@gmail.com>
2019-12-06 16:30:37 +0530

    Spring boot initialization

---
+++ databases-config/src/main/java/org/sunil/com/DatabasesConfigApplication.java
@@ -0,0 +1,13 @@
+package org.sunil.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabasesConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabasesConfigApplication.class, args);
+	}
+
+}
---
+++ databases-config/src/test/java/org/sunil/com/DatabasesConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sunil.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabasesConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
